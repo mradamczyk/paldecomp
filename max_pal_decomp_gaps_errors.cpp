c0077c0fa4c895ec32651335b30170f822ec87f8
Michał Adamczyk <mr.adamczyk@gmail.com>
2016-12-12 00:54:08 -0500

    fix indentations

--- max_pal_decomp_gaps_errors.cpp
+++ max_pal_decomp_gaps_errors.cpp
@@ -29,200 +29,200 @@ inline int dist(const pair<int, int> &p) {
 }
 
 class LCEStructure {
-private:
-    string y;
-    string x;
-    function<char(char)> f;
+    private:
+        string y;
+        string x;
+        function<char(char)> f;
 
-public:
-    LCEStructure(string t, function<char(char)> f) {
-        this->f = f;
+    public:
+        LCEStructure(string t, function<char(char)> f) {
+            this->f = f;
 
-	// x = #t
-        // y = x + $ + f(x^R)
-        this->y = "#";
-        this->y.append(t);
-        this->x = string(this->y);
-        for (auto &c: t) c = f(c);
-        std::reverse(std::begin(t), std::end(t));
-        this->y.append(t);
-    }
-
-    // TODO: implement sth better here
-    // LCE - brute
-    int LCE(int i, int j) {
-        int k = 0, n = this->x.size(), N = this->y.size();
-        while (i <= n && j < N && this->y[i] == this->y[j]) // TODO: pewnie i<=n niepotrzebne i moze jakis +-1 na j
-            ++i, ++j, ++k;
-        return k;
-    }
-
-    int LGPal(int i, int j) {
-        int k = 0, n = this->x.size();
-        while (i > 0 && j <= n && this->f(this->x[i]) == this->x[j]) --i, ++j, ++k;
-        return k;
-    }
-
-    // TODO: implement LGPal using LCE
-
-    // Finding maximal g-palindrome centered between i and j under Hamming dist
-    // For an even-length g-palindrome we set j=i+1
-    // For an odd-length g-palindrome we set j=i -- run only when f(t[i])=t[i]
-    int maxPalHam(int i, int j, int g) {
-        int a = i, b = j, d;
-        int n = this->x.size() - 1;
-        for (int s = 0; s <= g; ++s) {
-            if (a == 0 || b == n+1) return i - a;
-            d = LGPal(a, b);
-            a -= d;
-            b += d;
-            if (a == 0 || b == n+1) return i - a;
-            if (s < g)
-                --a, ++b;
+            // x = #t
+            // y = x + $ + f(x^R)
+            this->y = "#";
+            this->y.append(t);
+            this->x = string(this->y);
+            for (auto &c: t) c = f(c);
+            std::reverse(std::begin(t), std::end(t));
+            this->y.append(t);
         }
-        return i - a;
-    }
 
-    vector< pair<int, int> > allMaxPalHam(int g) {
-        int n = this->x.size() - 1;
-        vector< pair<int, int> > P;
-
-        int d;
-        for (int i = 1; i <= n; ++i) {
-            for (int k = 0; k < 2 && i + k <= n; ++k) {
-                d = maxPalHam(i, i+k, g);
-                P.push_back(make_pair(i-d+1, i+k+d-1));
-            }
+        // TODO: implement sth better here
+        // LCE - brute
+        int LCE(int i, int j) {
+            int k = 0, n = this->x.size(), N = this->y.size();
+            while (i <= n && j < N && this->y[i] == this->y[j]) // TODO: pewnie i<=n niepotrzebne i moze jakis +-1 na j
+                ++i, ++j, ++k;
+            return k;
         }
-        return P;
-    }
 
-    // Finding all maximal g-palindrome centered under Edit dist
-    // For an even-length g-palindrome we set j=i+1
-    // For an odd-length g-palindrome we set j=i -- run only when f(t[i])=t[i]
-    vector< pair<int, int> > allMaxPalEdit(int g) {
-        int n = this->x.size() - 1;
-        vector< pair<int, int> > P;
-        vector< pair<int, int> > buckets(2*n+1, make_pair(0,-1));
+        int LGPal(int i, int j) {
+            int k = 0, n = this->x.size();
+            while (i > 0 && j <= n && this->f(this->x[i]) == this->x[j]) --i, ++j, ++k;
+            return k;
+        }
 
-        // all max palindromes without errors
-        P = allMaxPalHam(0);
-        int d, i, j, i1, j1;
-        for (int k = 1; k <= g; ++k) {
-            // try to extend palindromes
-            for (auto &p: P) {
-                i = p.first, j = p.second;
-                for (const pair<int,int> &c: vector< pair<int, int> >{{1,0}, {0,1}, {1,1}}) {
-                    d = LGPal(i-1-c.first, j+1+c.second);
-                    i1 = i - c.first - d, j1 = j + c.second + d;
-                    addPalToBucketIfLonger(make_pair(i1, j1), buckets);
-                }
-                // Border reductions
-                if (i == 1)  addPalToBucketIfLonger(make_pair(1, j-1), buckets);
-                if (j == n)  addPalToBucketIfLonger(make_pair(i+1, n), buckets);
+        // TODO: implement LGPal using LCE
+
+        // Finding maximal g-palindrome centered between i and j under Hamming dist
+        // For an even-length g-palindrome we set j=i+1
+        // For an odd-length g-palindrome we set j=i -- run only when f(t[i])=t[i]
+        int maxPalHam(int i, int j, int g) {
+            int a = i, b = j, d;
+            int n = this->x.size() - 1;
+            for (int s = 0; s <= g; ++s) {
+                if (a == 0 || b == n+1) return i - a;
+                d = LGPal(a, b);
+                a -= d;
+                b += d;
+                if (a == 0 || b == n+1) return i - a;
+                if (s < g)
+                    --a, ++b;
             }
-		
-            P.clear();
-            // sort by center positions
-            for (int i = 2; i <= 2*n; ++i) {
-                if (dist(buckets[i]) > 0) {
-                    P.push_back(buckets[i]);
-                    buckets[i] = make_pair(0, -1);
+            return i - a;
+        }
+
+        vector< pair<int, int> > allMaxPalHam(int g) {
+            int n = this->x.size() - 1;
+            vector< pair<int, int> > P;
+
+            int d;
+            for (int i = 1; i <= n; ++i) {
+                for (int k = 0; k < 2 && i + k <= n; ++k) {
+                    d = maxPalHam(i, i+k, g);
+                    P.push_back(make_pair(i-d+1, i+k+d-1));
                 }
             }
-        }
-        return P;
-    }
-
-    inline string getPal(const pair<int, int> &t) {
-        if (t.first > t.second) return "";
-        return this->x.substr(t.first, t.second-t.first+1);
-    }
-
-    void addPalToBucketIfLonger(const pair<int, int> &temp, vector<pair<int, int>> &buckets) {
-        int mid = temp.first + temp.second;
-	int n = this->x.size() - 1;
-        if (temp.first >= 1 && temp.second <= n && mid <= 2*n && dist(buckets[mid]) < dist(temp)) {
-		std::cout << "2n = " << 2*n << ", i = " << mid << " " << temp.first << " " << temp.second << std::endl;
-        	buckets[mid] = temp;
-        }
-    }
-
-    vector<int> maxPalFactWithErrosGaps(const string &errorsMetric, int errorsAllowed, int minLength, int maxGaps) {
-        vector<pair<int, int>> Pal;
-        if (errorsMetric == "edit") { // Edit distance
-            Pal = allMaxPalEdit(errorsAllowed);
-        } else { // Hamming dist
-            Pal = allMaxPalHam(errorsAllowed);
+            return P;
         }
 
-        int n = this->x.size() - 1;
-        int INFTY = 2 * n;
+        // Finding all maximal g-palindrome centered under Edit dist
+        // For an even-length g-palindrome we set j=i+1
+        // For an odd-length g-palindrome we set j=i -- run only when f(t[i])=t[i]
+        vector< pair<int, int> > allMaxPalEdit(int g) {
+            int n = this->x.size() - 1;
+            vector< pair<int, int> > P;
+            vector< pair<int, int> > buckets(2*n+1, make_pair(0,-1));
 
-        vector<vector<int>> MG(n+1), MG1(n+1);
-        vector<vector<pair<int, int>>> D(n+1), D1(n+1);
-        for (int j = 0; j <= n; ++j) {
-            MG[j].resize(maxGaps+1);
-            MG1[j].resize(maxGaps+1);
-            D[j].resize(maxGaps+1);
-            D1[j].resize(maxGaps+1);
-            for (int q = 0; q <= maxGaps; ++q) {
-                if (j == 0) {
-                    MG[j][q] = 0;
-                    D[j][q] = make_pair(0,0);
-                } else {
-                    MG[j][q] = INFTY;
-                    D[j][q] = make_pair(j, q);
+            // all max palindromes without errors
+            P = allMaxPalHam(0);
+            int d, i, j, i1, j1;
+            for (int k = 1; k <= g; ++k) {
+                // try to extend palindromes
+                for (auto &p: P) {
+                    i = p.first, j = p.second;
+                    for (const pair<int,int> &c: vector< pair<int, int> >{{1,0}, {0,1}, {1,1}}) {
+                        d = LGPal(i-1-c.first, j+1+c.second);
+                        i1 = i - c.first - d, j1 = j + c.second + d;
+                        addPalToBucketIfLonger(make_pair(i1, j1), buckets);
+                    }
+                    // Border reductions
+                    if (i == 1)  addPalToBucketIfLonger(make_pair(1, j-1), buckets);
+                    if (j == n)  addPalToBucketIfLonger(make_pair(i+1, n), buckets);
                 }
-                MG1[j][q] = INFTY;
-                D1[j][q] = make_pair(j, q);
-            }
-        }
 
-        vector<vector<int> > P(n+1);
-        for (auto &p: Pal) P[p.second].push_back(p.first);
-
-        for (int j = 1; j <= n; ++j) {
-            for (int q = 0; q <= maxGaps; ++q) {
-                if (q > 0) {
-                    if (MG1[j-1][q] <= MG[j-1][q-1]) {
-                        MG1[j][q] = MG1[j-1][q] + 1;
-                        D1[j][q] = D1[j-1][q];
-                    } else {
-                        MG1[j][q] = MG[j-1][q-1] + 1;
-                        D1[j][q] = make_pair(j-1, q-1);
+                P.clear();
+                // sort by center positions
+                for (int i = 2; i <= 2*n; ++i) {
+                    if (dist(buckets[i]) > 0) {
+                        P.push_back(buckets[i]);
+                        buckets[i] = make_pair(0, -1);
                     }
                 }
-                MG[j][q] = MG1[j][q];
-                D[j][q] = D1[j][q];
-                for (int i: P[j])
-                    if (j - i + 1 >= minLength)
-                        if (MG[i-1][q] < MG[j][q]) {
-                            MG[j][q] = MG[i-1][q];
-                            D[j][q] = make_pair(i-1, q);
-                        }
             }
+            return P;
         }
-        if (print) {
-            int prevj = n, prevq = maxGaps;
-            int j, q;
-            j = D[prevj][prevq].first, q = D[prevj][prevq].second;
-            vector<int> rev;
-            while (prevj != j || prevq != q || (j != 0 && q != 0)) {
-                rev.push_back(prevj);
-                prevj = j, prevq = q;
-                j = D[prevj][prevq].first, q = D[prevj][prevq].second;
-            }
-            rev.push_back(0);
-            std::reverse(std::begin(rev), std::end(rev));
 
-            for (uint i = 0; i < rev.size() - 1; ++i) {
-                std::cout << x.substr(rev[i]+1, rev[i+1]-rev[i]) << " ";
-            }
-            std::cout << std::endl;
+        inline string getPal(const pair<int, int> &t) {
+            if (t.first > t.second) return "";
+            return this->x.substr(t.first, t.second-t.first+1);
         }
-        return MG[n];
-    }
+
+        void addPalToBucketIfLonger(const pair<int, int> &temp, vector<pair<int, int>> &buckets) {
+            int mid = temp.first + temp.second;
+            int n = this->x.size() - 1;
+            if (temp.first >= 1 && temp.second <= n && mid <= 2*n && dist(buckets[mid]) < dist(temp)) {
+                std::cout << "2n = " << 2*n << ", i = " << mid << " " << temp.first << " " << temp.second << std::endl;
+                buckets[mid] = temp;
+            }
+        }
+
+        vector<int> maxPalFactWithErrosGaps(const string &errorsMetric, int errorsAllowed, int minLength, int maxGaps) {
+            vector<pair<int, int>> Pal;
+            if (errorsMetric == "edit") { // Edit distance
+                Pal = allMaxPalEdit(errorsAllowed);
+            } else { // Hamming dist
+                Pal = allMaxPalHam(errorsAllowed);
+            }
+
+            int n = this->x.size() - 1;
+            int INFTY = 2 * n;
+
+            vector<vector<int>> MG(n+1), MG1(n+1);
+            vector<vector<pair<int, int>>> D(n+1), D1(n+1);
+            for (int j = 0; j <= n; ++j) {
+                MG[j].resize(maxGaps+1);
+                MG1[j].resize(maxGaps+1);
+                D[j].resize(maxGaps+1);
+                D1[j].resize(maxGaps+1);
+                for (int q = 0; q <= maxGaps; ++q) {
+                    if (j == 0) {
+                        MG[j][q] = 0;
+                        D[j][q] = make_pair(0,0);
+                    } else {
+                        MG[j][q] = INFTY;
+                        D[j][q] = make_pair(j, q);
+                    }
+                    MG1[j][q] = INFTY;
+                    D1[j][q] = make_pair(j, q);
+                }
+            }
+
+            vector<vector<int> > P(n+1);
+            for (auto &p: Pal) P[p.second].push_back(p.first);
+
+            for (int j = 1; j <= n; ++j) {
+                for (int q = 0; q <= maxGaps; ++q) {
+                    if (q > 0) {
+                        if (MG1[j-1][q] <= MG[j-1][q-1]) {
+                            MG1[j][q] = MG1[j-1][q] + 1;
+                            D1[j][q] = D1[j-1][q];
+                        } else {
+                            MG1[j][q] = MG[j-1][q-1] + 1;
+                            D1[j][q] = make_pair(j-1, q-1);
+                        }
+                    }
+                    MG[j][q] = MG1[j][q];
+                    D[j][q] = D1[j][q];
+                    for (int i: P[j])
+                        if (j - i + 1 >= minLength)
+                            if (MG[i-1][q] < MG[j][q]) {
+                                MG[j][q] = MG[i-1][q];
+                                D[j][q] = make_pair(i-1, q);
+                            }
+                }
+            }
+            if (print) {
+                int prevj = n, prevq = maxGaps;
+                int j, q;
+                j = D[prevj][prevq].first, q = D[prevj][prevq].second;
+                vector<int> rev;
+                while (prevj != j || prevq != q || (j != 0 && q != 0)) {
+                    rev.push_back(prevj);
+                    prevj = j, prevq = q;
+                    j = D[prevj][prevq].first, q = D[prevj][prevq].second;
+                }
+                rev.push_back(0);
+                std::reverse(std::begin(rev), std::end(rev));
+
+                for (uint i = 0; i < rev.size() - 1; ++i) {
+                    std::cout << x.substr(rev[i]+1, rev[i+1]-rev[i]) << " ";
+                }
+                std::cout << std::endl;
+            }
+            return MG[n];
+        }
 };
 
 int main() {
--- min_fact.cpp
+++ min_fact.cpp
@@ -10,13 +10,13 @@
 
 inline char standardPalindrom(const char &t) { return t; }
 inline char dnaComplementaryPalindrom(const char &t) {
-	switch (t) {
-		case 'A': return 'T';
-		case 'C': return 'G';
-		case 'G': return 'C';
-		case 'T': return 'A';
-	}
-	return t;
+    switch (t) {
+        case 'A': return 'T';
+        case 'C': return 'G';
+        case 'G': return 'C';
+        case 'T': return 'A';
+    }
+    return t;
 }
 
 using triple = std::tuple<int, int, int>;
@@ -29,129 +29,129 @@ using std::vector;
 using std::function;
 
 int minPalFactFICI(const string &t, function<char(char)> f) {
-	std::cerr << "FICI"  << std::endl;
-	int n = t.size() - 1;
+    std::cerr << "FICI"  << std::endl;
+    int n = t.size() - 1;
 
-	vector<int> PL(n + 1, 2*n);
-	vector<int> GPL(n + 1, 2*n);
-	vector<triple> G, G1;
-	std::queue<triple> G2;
-	int i, d, k;
-	int i1, d1, k1;
+    vector<int> PL(n + 1, 2*n);
+    vector<int> GPL(n + 1, 2*n);
+    vector<triple> G, G1;
+    std::queue<triple> G2;
+    int i, d, k;
+    int i1, d1, k1;
 
-	PL[0] = 0;
-	G.clear();
+    PL[0] = 0;
+    G.clear();
 
-	for (int j = 1; j <= n; ++j) {
-		G1.clear();
-		for (const triple &g : G) {
-			tie(i, d, k) = g;
-			if (i > 1 && t[i-1] == f(t[j]))
-				G1.push_back(make_tuple(i-1,d,k));
-		}
+    for (int j = 1; j <= n; ++j) {
+        G1.clear();
+        for (const triple &g : G) {
+            tie(i, d, k) = g;
+            if (i > 1 && t[i-1] == f(t[j]))
+                G1.push_back(make_tuple(i-1,d,k));
+        }
 
-		int r = -j; // i-r big enough to act as inf
-		for (const triple &g : G1) {
-			tie(i, d, k) = g;
-			if (i - r != d) {
-				G2.push(make_tuple(i,i-r,1));
-				if (k > 1)
-					G2.push(make_tuple(i+d, d, k-1));
-			} else {
-				G2.push(make_tuple(i,d,k));
-			}
-			r = i + (k-1)*d;
-		}
-		if (j > 1 && t[j-1] == f(t[j])) {
-			G2.push(make_tuple(j-1, j-1-r, 1));
-			r = j-1;
-		}
-		if (t[j] == f(t[j]))
-			G2.push(make_tuple(j, j-r, 1));
+        int r = -j; // i-r big enough to act as inf
+        for (const triple &g : G1) {
+            tie(i, d, k) = g;
+            if (i - r != d) {
+                G2.push(make_tuple(i,i-r,1));
+                if (k > 1)
+                    G2.push(make_tuple(i+d, d, k-1));
+            } else {
+                G2.push(make_tuple(i,d,k));
+            }
+            r = i + (k-1)*d;
+        }
+        if (j > 1 && t[j-1] == f(t[j])) {
+            G2.push(make_tuple(j-1, j-1-r, 1));
+            r = j-1;
+        }
+        if (t[j] == f(t[j]))
+            G2.push(make_tuple(j, j-r, 1));
 
-		G.clear();
-		if (!G2.empty()) {
-			tie(i1, d1, k1) = G2.front();
-			G2.pop();
-			while (!G2.empty()) {
-				tie(i,d,k) = G2.front();
-				G2.pop();
-				if (d1 == d)
-					k1 += k;
-				else {
-					G.push_back(make_tuple(i1,d1,k1));
-					i1 = i, d1 = d, k1 = k;
-				}
-			}
-			G.push_back(make_tuple(i1, d1, k1));
-		}
+        G.clear();
+        if (!G2.empty()) {
+            tie(i1, d1, k1) = G2.front();
+            G2.pop();
+            while (!G2.empty()) {
+                tie(i,d,k) = G2.front();
+                G2.pop();
+                if (d1 == d)
+                    k1 += k;
+                else {
+                    G.push_back(make_tuple(i1,d1,k1));
+                    i1 = i, d1 = d, k1 = k;
+                }
+            }
+            G.push_back(make_tuple(i1, d1, k1));
+        }
 
-		for (const triple &g : G) {
-			tie(i, d, k) = g;
+        for (const triple &g : G) {
+            tie(i, d, k) = g;
 
-			r = i + (k-1) * d;
-			int m = PL[r-1] + 1;
-			if (k > 1)
-				m = min(m, GPL[i - d]);
-			if (d <= i)
-				GPL[i-d] = m;
-			PL[j] = min(PL[j], m);
-		}
-	}
-	return PL[n] > n ? -1 : PL[n];
+            r = i + (k-1) * d;
+            int m = PL[r-1] + 1;
+            if (k > 1)
+                m = min(m, GPL[i - d]);
+            if (d <= i)
+                GPL[i-d] = m;
+            PL[j] = min(PL[j], m);
+        }
+    }
+    return PL[n] > n ? -1 : PL[n];
 }
 
 int minPalFactN2(const string &t, function<char(char)> f) {
-	std::cerr << "BRUTE"  << std::endl;
+    std::cerr << "BRUTE"  << std::endl;
 
-	int n = t.size() - 1;
-	vector<int> PL(n+1, 2*n);
-	vector<int> P[2];
+    int n = t.size() - 1;
+    vector<int> PL(n+1, 2*n);
+    vector<int> P[2];
 
-	PL[0] = 0;
-	P[0].clear();
-	for (int j = 1; j <= n; ++j) {
-		P[j&1].clear();
-		for (int i: P[1-(j&1)])
-			if (i > 1 && t[i-1] == f(t[j]))
-				P[j&1].push_back(i-1);
+    PL[0] = 0;
+    P[0].clear();
+    for (int j = 1; j <= n; ++j) {
+        P[j&1].clear();
+        for (int i: P[1-(j&1)])
+            if (i > 1 && t[i-1] == f(t[j]))
+                P[j&1].push_back(i-1);
 
-		if (j > 1 && t[j-1] == f(t[j]))
-			P[j&1].push_back(j-1);
+        if (j > 1 && t[j-1] == f(t[j]))
+            P[j&1].push_back(j-1);
 
-		if (t[j] == f(t[j]))
-			P[j&1].push_back(j);
+        if (t[j] == f(t[j]))
+            P[j&1].push_back(j);
 
-		for (int i: P[j&1])
-			PL[j] = min(PL[j], PL[i-1] + 1);
-	}
-	return PL[n] > n ? -1 : PL[n];
+        for (int i: P[j&1])
+            PL[j] = min(PL[j], PL[i-1] + 1);
+    }
+    return PL[n] > n ? -1 : PL[n];
 }
 
 
 int main(int argc, char **argv) {
-	int opt, brute, dna;
-	brute = dna = 0;
-	while ((opt = getopt(argc,argv,"bd")) != EOF) {
-		switch(opt) {
-			case 'b': brute = 1; break;
-			case 'd': dna = 1; break;
-			case '?': fprintf(stderr, "usuage is \n -b : for running brute \n -d : for DNA complement palindromes [default: standard palindromes]"); break;
-		}
-	}
+    int opt, brute, dna;
+    brute = dna = 0;
+    while ((opt = getopt(argc,argv,"bd")) != EOF) {
+        switch(opt) {
+            case 'b': brute = 1; break;
+            case 'd': dna = 1; break;
+            case '?': fprintf(stderr, "usuage is \n -b : for running brute \n -d : for DNA complement palindromes [default: standard palindromes]"); break;
+        }
+    }
 
 
-	function<char(char)> f = dna ? dnaComplementaryPalindrom : standardPalindrom;
-	function<int(string, function<char(char)>)> minPalFact = brute ? minPalFactN2 : minPalFactFICI;
+    function<char(char)> f = dna ? dnaComplementaryPalindrom : standardPalindrom;
+    function<int(string, function<char(char)>)> minPalFact = brute ? minPalFactN2 : minPalFactFICI;
 
-	string t;
-	while (std::cin >> t) {
-		for (auto &c: t) c = toupper(c);
+    string t;
+    while (std::cin >> t) {
+        for (auto &c: t) c = toupper(c);
 
-		string s = "#";
-		s.append(t);
-	
-		std::cout << minPalFact(s, f) << std::endl;
-	}
-	return 0;
+        string s = "#";
+        s.append(t);
+
+        std::cout << minPalFact(s, f) << std::endl;
+    }
+    return 0;
 }
