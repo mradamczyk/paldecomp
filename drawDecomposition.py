3d3aa6329352bf2eb5f1d672b6c1f51212170ef0
Michał Adamczyk <mr.adamczyk@gmail.com>
2017-03-26 23:28:53 -0400

    change drawing of deletions at the beginning/end of word

--- drawDecomposition.py
+++ drawDecomposition.py
@@ -39,13 +39,15 @@ def word2nodes(f, word, delta):
         if deletion:
             last[2].append(let)
         else:
-            last = (let, swap, []) 
+            last = (let, swap, [])
             nodes.append(last)
+    nodes.append(('#', False, []))
     return nodes
 
 def decomposition2graph(f, decomposition, delta):
     g = nx.Graph()
     words = decomposition.split(' ')
+    #words = ['a','b','c']
     cnt = 1
     g.add_node(0, label='#', pos=[0, 0], color="white")
     last_x, last_y = 0, 0
@@ -58,141 +60,82 @@ def decomposition2graph(f, decomposition, delta):
             last_x += 1 + (int(len(w)/5))
         else:
             nodes = word2nodes(f, w, delta)
-
-            # deletions at the beginning of the word
-            if len(nodes[0][2]) == 0:
-                last_x += 1
-            elif len(nodes[0][2]) == 1:
-                last_x += 1
-                last_y -= 2
-                g.add_node(cnt, label=nodes[0][2][0], pos=[last_x, last_y], color='red')
-                g.add_edge(cnt-1, cnt, style='dashed')
-                cnt += 1
-                last_y += 2
-            else:
-                last_x += 0
-                last_y -= 1
-                k = (len(nodes[0][2]) + 1) / 2
-                for i in xrange(k):
-                    g.add_node(cnt, label=nodes[0][2][i], pos=[last_x, last_y], color='red')
-                    g.add_edge(cnt-1, cnt, style='dashed')
-                    last_y -= 1
-                    cnt += 1
-
-                last_x += 1
-                if len(nodes[0][2]) % 2:
-                    last_y += 1
-
-                for i in xrange(k, len(nodes[0][2])):
-                    g.add_node(cnt, label=nodes[0][2][i], pos=[last_x, last_y], color='red')
-                    g.add_edge(cnt-1, cnt, style='dashed')
-                    last_y += 1
-                    cnt += 1
-
-                last_x += 1
-
-            s = nodes[1:]
-            k = len(s) / 2
-
+            k = len(nodes) / 2
             temp = []
+            last_x += 1
             for i in xrange(k):
-                (let1, swap1, deletions1) = s[i]
-                (let2, swap2, deletions2) = s[i+k]
+                (let1, swap1, deletions1) = nodes[i]
+                (let2, swap2, deletions2) = nodes[-(i+2)]
 
                 temp.append((cnt, last_y))
                 mx = max(1, max(len(deletions1), len(deletions2)))
+
+                if i == 0 and not deletions1 and not deletions2:
+                    continue
 
                 g.add_node(cnt, label=let1, pos=[last_x, last_y], color='blue' if swap1 else 'black')
                 g.add_edge(cnt-1, cnt, style='solid')
                 cnt += 1
 
                 initial_y = last_y
+                last_y += max(0, len(deletions2) - len(deletions1)) * 2
                 if deletions1:
-                    last_x += 1 if i == k-1 else -1
-                    last_y += 1
+                    last_x += 1 if i == k-1 else (0 if i == 0 else -1)
+                    last_y += 1 if i != 0 else 0
                     for let in deletions1:
                         g.add_node(cnt, label=let, pos=[last_x, last_y], color='red')
                         g.add_edge(cnt-1, cnt, style='solid')
                         last_y += 2
                         cnt += 1
-                    last_y += 1
-                    last_x -= 1 if i == k-1 else -1
-                last_y = initial_y + mx * 2
+                    last_y += 1 if i != 0 else 2
+                    last_x -= 1 if i == k-1 else (0 if i == 0 else -1)
+                last_y = initial_y + mx * 2 - (0 if i != 0 or deletions1 or deletions2 else 2)
 
             last_x += 2
 
-            for i in xrange(k-1):
-                (let2, swap2, deletions2) = s[i+k]
+            for i in xrange(k):
+                (let2, swap2, deletions2) = nodes[i+k]
                 (r_id, r_y) = temp.pop()
+
+                if i == k-1 and len(nodes[-2][2]) >= len(nodes[0][2]):
+                    last_y = r_y
+                    continue
+
                 g.add_node(cnt, label=let2, pos=[last_x, r_y], color='blue' if swap2 else 'black')
                 g.add_edge(cnt-1, cnt, style='solid')
-                if r_id != cnt-1:
+                if r_id != cnt-1 and i != k-1:
                     g.add_edge(r_id, cnt, style='dotted')
                 cnt += 1
 
                 last_y = r_y
                 if deletions2:
-                    last_x += 1
-                    last_y -= 1
+                    last_x += 1 if i != k-2 else 0
+                    last_y -= 1 if i != k-2 else 2
                     for let in deletions2:
                         g.add_node(cnt, label=let, pos=[last_x, last_y], color='red')
                         g.add_edge(cnt-1, cnt, style='solid')
                         last_y -= 2
                         cnt += 1
-                    last_y -= 1
-                    last_x += 1 if i == k-1 else -1
-
-            (let2, swap2, deletions2) = nodes[-1]
-            (r_id, r_y) = temp.pop()
-            g.add_node(cnt, label=let2, pos=[last_x, r_y], color='blue' if swap2 else 'black')
-            g.add_edge(cnt-1, cnt, style='solid')
-            g.add_edge(r_id, cnt, style='dotted')
-            cnt += 1
-
-            last_y = r_y
-            # deletions at the end of the word
-            if len(nodes[-1][2]) == 0:
-                last_x += 2
-            elif len(nodes[-1][2]) == 1:
-                last_x += 1
-                last_y -= 1
-                g.add_node(cnt, label=nodes[-1][2][0], pos=[last_x, last_y], color='red')
-                g.add_edge(cnt-1, cnt, style='dashed')
-                cnt += 1
-                last_x += 1
-                last_y += 1
-            else:
-                last_x += 0
-                last_y -= 1
-                k = (len(nodes[-1][2]) + 1) / 2
-                for i in xrange(k):
-                    g.add_node(cnt, label=nodes[-1][2][i], pos=[last_x, last_y], color='red')
-                    g.add_edge(cnt-1, cnt, style='dashed')
-                    last_y -= 1
-                    cnt += 1
-
-                last_x += 1
-                if len(nodes[-1][2]) % 2:
-                    last_y += 1
-
-                for i in xrange(k, len(nodes[-1][2])):
-                    g.add_node(cnt, label=nodes[-1][2][i], pos=[last_x, last_y], color='red')
-                    g.add_edge(cnt-1, cnt, style='dashed')
-                    last_y += 1
-                    cnt += 1
-
-                last_x += 1
-            last_x += 1
+                    last_y -= 1 if i != k-2 else 0
+                    last_x += 1 if i == k-1 else (0 if i == k-2 else -1)
+            last_x += 2
     return g
 
 def drawGraph(g, fileName):
     G = gv.Graph(engine='neato', format='svg')
     G.graph_attr['size'] = '10'
+    for (x, y, values) in g.edges(data=True):
+        if x != 0 and y != 0:
+            G.edge("%d" % x, "%d" % y, style=values["style"])
+
     for node, values in g.nodes(data=True):
         (x, y) = values["pos"]
-        G.node("%d" % node, values["label"], fontsize='40', style='bold', bgcolor=values.get('fillcolor', 'white'), color=values.get('color', 'black'), pos="%d,%d!" % (x, y), shape=values.get('shape', 'circle'))
-    for (x, y, values) in g.edges(data=True):
-        G.edge("%d" % x, "%d" % y, style=values["style"])
+        if node != 0:
+            if values["label"] == '#':
+                G.node("%d" % node, fontsize='1', style='invisible', pos="%d,%d!" % (x, y), shape='point')
+            else:
+                G.node("%d" % node, values["label"], fontsize='40', style='filled', fillcolor='white', color=values.get('color', 'black'), pos="%d,%d!" % (x, y), shape=values.get('shape', 'circle'))
+
     G.render(fileName)
 
 def main(argv):
