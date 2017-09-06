8751d88d6e3f5369cf019a292e47b1ad300662b9
Michal Adamczyk <madamczyk@palantir.com>
2017-09-06 09:18:34 +0100

    eertree

--- src/pal_decomp_gaps.cpp
+++ src/pal_decomp_gaps.cpp
@@ -8,6 +8,7 @@
 #include<unistd.h>
 #include "relations.hpp"
 #include "DecompositionWithGaps/Palindromic/PalindromicDecompositionWithGapsBruteImpl.hpp"
+#include "DecompositionWithGaps/Palindromic/PalindromicDecompositionWithGapsEertreeImpl.hpp"
 #include "DecompositionWithGaps/Palindromic/PalindromicDecompositionWithGapsFastImpl.hpp"
 
 using triple = std::tuple<int, int, int>;
@@ -23,12 +24,13 @@ using std::vector;
 using std::function;
 
 int main(int argc, char **argv) {
-    int opt, brute, dna, minLength = 1, maxGapsNum = 0, print = 0;
+    int opt, brute, dna, minLength = 1, maxGapsNum = 0, print = 0, eertree = 0;
     brute = dna = 0;
-    while ((opt = getopt(argc,argv,"bdhpL:G:")) != EOF) {
+    while ((opt = getopt(argc,argv,"bdehpL:G:")) != EOF) {
         switch(opt) {
             case 'b': brute = 1; break;
             case 'd': dna = 1; break;
+            case 'e': eertree = 1; break;
             case 'p': print = 1; break;
             case 'L': minLength = atoi(optarg); break;
             case 'G': maxGapsNum = atoi(optarg); break;
@@ -37,6 +39,7 @@ int main(int argc, char **argv) {
                 fprintf(stderr, "usage is \n"
                         "-b : for running brute \n"
                         "-d : for DNA complement palindromes [default: standard palindromes]\n"
+                        "-e : for running eertree-based solution \n"
                         "-p : print decomposition (works only with -b)\n"
                         "-L X: set minimum palindrome length to X [default: 1]\n"
                         "-G X: set maximum allowed number of gaps to X [default: 0]\n");
@@ -52,6 +55,8 @@ int main(int argc, char **argv) {
         PalindromicDecompositionWithGaps *solver;
         if (brute) {
             solver = new PalindromicDecompositionWithGapsBruteImpl(t, f, minLength, maxGapsNum);
+        } else if (eertree) {
+            solver = new PalindromicDecompositionWithGapsEertreeImpl(t, f, minLength, maxGapsNum);
         } else {
             solver = new PalindromicDecompositionWithGapsFastImpl(t, f, minLength, maxGapsNum);
         }
