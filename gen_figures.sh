341c06e96b8b1f29497a92b0d59efd0ebaa246e3
Michal Adamczyk <madamczyk@palantir.com>
2017-09-12 09:36:06 +0200

    fix drawDecomposition for hamming and standard palindromes

--- gen_figures.sh
+++ gen_figures.sh
@@ -1,4 +1,4 @@
-./max_pal_decomp_gaps_errors.x -d -G 4 -E 3 -L 14 -H -p < examples/ab220944.in | python drawDecomposition.py -d -E 3 -f max-pal-ham
+./max_pal_decomp_gaps_errors.x -d -G 4 -E 3 -L 14 -H -p < examples/ab220944.in | python drawDecomposition.py -d -H -E 3 -f max-pal-ham
 ./max_pal_decomp_gaps_errors.x -d -G 4 -E 3 -L 14 -p < examples/ab220944.in | python drawDecomposition.py -d -E 3 -f max-pal-edit-1
 ./max_pal_decomp_gaps_errors.x -d -G 4 -E 4 -L 20 -p < examples/ab220944.in | python drawDecomposition.py -d -E 4 -f max-pal-edit-2
 
